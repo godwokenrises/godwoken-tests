26fc109329b7736840aec7e28e4e07596fb7b9d9
Flouse <flouse@gmail.com>
2021-05-21 14:18:24 +0800

    get account_id and script hash after deposition

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,3 +1,12 @@
 pub mod specs;
 pub use specs::Spec;
 pub mod worker;
+
+/// The 32-byte fixed-length binary data, Represent 256 bits
+/// 
+/// The name comes from the number of bits in the data.
+///
+/// In JSONRPC, it is encoded as a 0x-prefixed hex string.
+#[derive(Eq, PartialEq, Debug, Default, Hash, Clone, Copy)]
+pub struct H256([u8; 32]);
+// pub struct H256(pub [u8; 32]);
--- tests/src/specs/ckb_asset.rs
+++ tests/src/specs/ckb_asset.rs
@@ -1,7 +1,8 @@
+use anyhow::{anyhow, Result};
 use std::process::{Command};
 use crate::{Spec};
 use std::env;
-use regex::Regex;
+use regex::{Regex};
 //TODO: https://docs.rs/env_logger/0.8.3/env_logger/ 
 //TODO: Redirect both stdout and stderr of child process to the same file
 
@@ -10,18 +11,22 @@ pub const CKB_SUDT_ACCOUNT_ID: u32 = 1;
 pub struct GodwokenUser {
 	private_key: String,
 	pub_ckb_addr: String,
-	gw_account_id: u32,
+	gw_account_id: Option<u32>,  // FIXME: get account_id
 	ckb_balance: u128,
-	// account_script_hash: H256
+	account_script_hash: Option<String>
 	//TODO: sudt_balance[]
 }
 
 impl GodwokenUser {
-	fn get_balance(&mut self) -> u128{
+	fn get_balance(&mut self) -> Result<u128>{
+		if self.gw_account_id.is_none() {
+			// println!("missing gw_account_id");
+			return Err(anyhow!("Missing gw_account_id: {:?}", self.gw_account_id));
+		}
 		// FIXME: get gw_account_id
 		let pattern: Regex = Regex::new(r"[B|b]alance: (\d+)").unwrap();
 		let mut balance_output = account_cli()
-		  .args(&["get-balance", &self.gw_account_id.to_string()])
+		  .args(&["get-balance", &self.gw_account_id.unwrap().to_string()])
 			.output()
 			.expect("failed to get-balance");
 		let output_text = String::from_utf8(balance_output.stdout)
@@ -30,7 +35,7 @@ impl GodwokenUser {
 			if cap.len() > 1 { cap.get(1).unwrap().as_str() } else { "0" }
 		} else { panic!("can't get balance.") };
 		self.ckb_balance = u128::from_str_radix(balance_str, 10).unwrap();
-		self.ckb_balance		
+		Ok(self.ckb_balance)
 	}
 }
 
@@ -42,7 +47,6 @@ impl Spec for CkbAsset {
 	///		2. godwoken transfer from MINER to USER1
 	///   3. withdraw CKB from layer2 to layer1
 	fn run(&self) {
-		// call account-cli
 		println!("\nCkbAsset Test Case: invoke account-cli to deposit -> transfer -> withdraw");
 
 		let ckb_rpc: String = env::var("CKB_RPC")
@@ -54,7 +58,8 @@ impl Spec for CkbAsset {
 			pub_ckb_addr: env::var("MINER_CKB_ADDR")
 				.unwrap_or("ckt1qyqy84gfm9ljvqr69p0njfqullx5zy2hr9kq0pd3n5".to_string()),
 			ckb_balance: 0,
-			gw_account_id: 2 // FIXME: get account_id
+			account_script_hash: None,
+			gw_account_id: None
 		};
 		let mut user1 = GodwokenUser {
 			private_key: env::var("USER1_PRIVATE_KEY")
@@ -62,55 +67,70 @@ impl Spec for CkbAsset {
 			pub_ckb_addr: env::var("USER1_CKB_ADDR")
 				.unwrap_or("ckt1qyqf22qfzaer95xm5d2m5km0f6k288x9warqnhsf4m".to_string()),
 			ckb_balance: 0,
-			gw_account_id: 3 // FIXME: get account_id
+			account_script_hash: None,
+			gw_account_id: None
 		};
 
-		//TODO: getAccountIdByScriptHash
-		// using eth address: 0x0c1efcca2bcb65a532274f3ef24c044ef4ab6d73
-		// rollupTypeHash: 0xf70aa98a96fba847185be1b58c1d1e3cae7ad91f971eecc5749799d5e72939f0
-		// Layer 2 lock script hash: 0xfe4fd59fb87ee629a81f0f1120cd0be244bf39860559ccfb65c1379bf9942941
-		// ↑ Using this script hash to get user account id ↑
-// 		gw-cli --help                            ─╯
-// Usage: godwoken-cli [options] [command]
 
-// Options:
-//   -r, --rpc <rpc>                                                                                Godwoken jsonrpc url (default: "http://127.0.0.1:8119")
-//   -s, --ckb-rpc <ckbRpc>                                                                         CKB node jsonrpc url (default: "http://127.0.0.1:8114")
-//   -h, --help                                                                                     display help for command
+		// call account-cli to deposit
+    //TODO: deposit and get the script hash when the deposition finished.
 
-// Commands:
-//   getNonce <account_id>                                                                          Get nonce from account
-//   getBalance <sudt_id> <account_id>                                                              Get balance from account
-//   generateTransactionMessageToSign <from_id> <to_id> <nonce> <args> <rollup_type_hash>           Generate the raw layer 2 transaction message to sign
-//   createAccountRawL2Transaction <from_id> <nonce> <script_code_hash> <script_args>               Create raw layer 2 transaction for layer 2 creator account (to_id)
-//   createAccount <from_id> <nonce> <script_code_hash> <script_args> <rollup_type_hash> <privkey>  Create an account by target script
-//   executeL2Transaction <from_id> <to_id> <nonce> <args> <signature>                              Submit the layer 2 transaction
-//   submitL2Transaction <from_id> <to_id> <nonce> <args> <signature>                               Submit the layer 2 transaction
-//   signMessage <message> <privkey>                                                                Sign the message use secp256k1
-//   getAccountIdByScriptHash <script_hash>                                                         Get account id by script hash
-//   getScriptHash <account_id>                                                                     Get script hash by account id
-//   getScript <script_hash>                                                                        Get script by script hash
-//   deposite <privkey> <amount>                                                                    Deposite some value [TODO]
-//   unlockWithdraw <privkey> <runner_config>                                                       Unlock one finalized withdrawal locked cell (NOTE: need lumos-config path)
-//   help [command]                                                                                 display help for command
-
-
-		let mut miner_balance_record = miner.get_balance();
-		let mut user1_balance_record = user1.get_balance();
-		println!("miner_balance_record: {}", miner_balance_record);
-		println!("user1_balance_record: {}", user1_balance_record);
-
-		return;
-
-		let _exit_status: std::process::ExitStatus = account_cli()
+		let mut output = account_cli()
 			.arg("deposit")
 			.args(&["--rpc", &ckb_rpc])
 			.args(&["-p", &miner.private_key])
 			.args(&["-c", "60000000000"]) // 600 CKBytes = 60,000,000,000 Shannons
-			.status()
+			.output()
 			.expect("failed to deposit CKB from layer1 to layer2");
-		//TODO: if _exit_status.status.success()
 
+		let script_hash_pattern = Regex::new(r"script hash: (0x.{64})").unwrap();
+		let ckb_balance_pattern = Regex::new(r"ckb balance in godwoken is: (\d+)").unwrap();
+	  let account_id_pattern = Regex::new(r"Your account id: (\d+)").unwrap();
+
+		miner.ckb_balance = String::from_utf8(output.stdout).unwrap()
+			.lines()
+			.filter_map(|line| {
+				println!("{}", line);
+				// update account_script_hash from log
+				if miner.account_script_hash.is_none() {
+					if let Some(cap) = script_hash_pattern.captures(line) {
+						miner.account_script_hash = Some(cap.get(1).unwrap().as_str().to_string());
+					}
+					println!("update miner.account_script_hash to {:?}", miner.account_script_hash);
+				}
+				// update gw_account_id 
+				if miner.gw_account_id.is_none() {
+					if let Some(cap) = account_id_pattern.captures(line) {
+						miner.gw_account_id = cap.get(1).unwrap().as_str().parse::<u32>().ok();
+					}
+					println!("update miner.gw_account_id to {:?}", miner.gw_account_id);
+				}
+				ckb_balance_pattern.captures(line)
+			}) // update ckb balance
+			.last().unwrap().get(1).unwrap().as_str().parse::<u128>().unwrap();
+			println!("udpate miner.ckb_balance to {:?}", miner.ckb_balance);
+		
+	
+		// let first_balance  = if let Some(cap) = balance_lines.next() {			
+		// 	u128::from_str_radix(cap.get(1).unwrap().as_str(), 10).unwrap()
+		// } else { panic!("get first_balance error") };
+
+		// let last_balance = if let Some(cap) = balance_lines.last() {
+		// 	u128::from_str_radix(cap.get(1).unwrap().as_str(), 10).unwrap()
+		// } else { panic!("get last_balance error") };
+		// assert_eq!(last_balance - first_balance, 60000000000);
+		// miner.ckb_balance = last_balance;
+
+		
+		//TODO: if _exit_status.status.success()
+		
+
+		let mut miner_balance_record = miner.get_balance().unwrap();
+		println!("miner_balance_record: {}", miner_balance_record);
+		let mut user1_balance_record = user1.get_balance().unwrap();
+		println!("user1_balance_record: {}", user1_balance_record);
+
+		return;
 
 		// transfer
 		println!("\nTransfer 10001 Shannons from ID:2 to ID:3");
