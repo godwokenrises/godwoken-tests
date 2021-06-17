90ee35b860a803bc63ce27fbf6e247202a2ef1a2
Flouse <flouse@gmail.com>
2021-06-17 18:37:21 +0800

    refactor: withdrawal wait_time = finality_blocks * 3
    * https://github.com/nervosnetwork/godwoken-tests/blob/acce7493ea3459513a99982460dcd4e0c7eda63c/configs/godwoken-config.toml#L35

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Serialize};
+
 /// The 32-byte fixed-length binary data, Represent 256 bits
 ///
 /// The name comes from the number of bits in the data.
@@ -15,3 +17,18 @@ pub const CKB_SUDT_ID: u32 = 1;
 pub const CKB_SUDT_SCRIPT_HASH: &str =
     "0x0000000000000000000000000000000000000000000000000000000000000000";
 pub const X_SUDT_ID: u32 = 2;
+
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Config {
+    pub genesis: GenesisConfig,
+}
+
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+pub struct GenesisConfig {
+    pub rollup_config: RollupConfig,
+}
+
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+pub struct RollupConfig {
+    pub finality_blocks: String,
+}
