3ed15aa040b67b8c39b0f033261955909e05919c
Flouse <flouse@gmail.com>
2022-01-06 23:12:00 +0800

    fix(account-analysis): cache the account data

--- scripts/account-analysis/src/index.ts
+++ scripts/account-analysis/src/index.ts
@@ -1,3 +1,4 @@
+import { promises as fs } from "fs";
 import { Godwoker } from '@polyjuice-provider/base';
 import { HexString, Script } from "@ckb-lumos/base";
 import { retry, sleep } from './utils';
@@ -5,8 +6,9 @@ import { Logger } from "@ethersproject/logger";
 import path from "path";
 import dotenv from "dotenv";
 
+const { readFile, writeFile } = fs;
 const logger = Logger.globalLogger();
-const INTERVAL = 50; // ms
+const INTERVAL = 100; // ms
 
 dotenv.config({
   path: path.resolve(process.env.ENV_PATH ?? "./testnet.env"),
@@ -53,6 +55,7 @@ type AccountAnalysisResult = {
 interface IAccount {
   id: number;
   scriptHash?: HexString;
+  script?: Script;
   type?: AccountType;
 }
 class Account implements IAccount {
@@ -61,8 +64,11 @@ class Account implements IAccount {
   script?: Script;
   type?: AccountType;
 
-  constructor(id: number) {
+  constructor({ id, scriptHash, script, type }: IAccount) {
     this.id = id;
+    this.scriptHash = scriptHash;
+    this.script = script;
+    this.type = type;
   }
 
   async getScript(): Promise<Script> {
@@ -91,29 +97,56 @@ class Account implements IAccount {
     return this.scriptHash;
   }
 }
-const accounts: Account[] = [];
+const accounts = new Map();
 
 async function getGodwokenInfo() {
+  logger.info(`Analyze ${process.env.GW_NET}...`);
   console.debug("creator account ID:", await godwoker.getPolyjuiceCreatorAccountId());
 }
 
-async function initAccounts() {
-  // TODO: fetch max_account_id
-  const maxAccountId = 10000;
+const cacheFilePath = `dist/account_data_${process.env.GW_NET}.json`;
 
+async function saveAccountData(): Promise<void> {
+  const accountData: IAccount[] = [];
+  accounts.forEach(account => accountData.push(account));
+  const jsonData = JSON.stringify(accountData, null, 2);
+  return writeFile(cacheFilePath, jsonData, "utf-8");
+}
+
+async function loadAccountData(): Promise<void> {
+  const accountJsonData = await readFile(cacheFilePath, "utf-8")
+    .catch(err => {
+      if (err.code === "ENOENT") {
+        logger.warn("No such file");
+      } else logger.warn(err);
+      return "[]";
+    });
+  const accountData =  JSON.parse(accountJsonData) as IAccount[];
+  accountData.forEach(account =>
+    accounts.set(account.id, new Account(account))
+  );
+}
+
+async function initAccounts() {
+  await loadAccountData();
+
+  const maxAccountId = parseInt(process.env.MAX_ACCOUNT_ID);
   let stopSignal = false;
   for (let id = 0; id < maxAccountId; id++) {
-    accounts.push(new Account(id));
-    accounts[id].getScriptHash().then(scriptHash => {
+    if (accounts.has(id)) continue;
+
+    accounts.set(id, new Account({id}));
+    accounts.get(id).getScriptHash().then(scriptHash => {
       logger.debug(`scriptHash of account ${id} is ${scriptHash}`);
     });
-    await sleep(INTERVAL);
 
+    await sleep(INTERVAL);
     // TODO: stopSignal
     if (stopSignal) {
       break;
     }
   }
+  saveAccountData();
 }
 
 (async function analyze(): Promise<void> {
@@ -133,9 +166,14 @@ async function initAccounts() {
 
   let idx = -1;
   const polyCreatorId = await godwoker.getPolyjuiceCreatorAccountId();
-  while (++idx < accounts.length) { // check the type of all accounts
-    if (await accounts[idx].getScriptHash() === ZERO_HASH) {
-      accounts[idx].type = AccountType.ZERO;
+  while (++idx < accounts.size) { // check the type of all accounts
+    if (!accounts.has(idx)) throw new Error("account id error");
+
+    const account: Account = accounts.get(idx);
+    if (account.type) continue;
+
+    if (await account.getScriptHash() === ZERO_HASH) {
+      account.type = AccountType.ZERO;
       result[AccountType.ZERO]++;
       logger.warn(`Account_${idx}'s scriptHash is ZERO_HASH`);
       continue;
@@ -143,37 +181,37 @@ async function initAccounts() {
 
     // detect creator account
     if (parseInt(polyCreatorId) === idx) {
-      accounts[idx].type = AccountType.CREATOR;
+      account.type = AccountType.CREATOR;
       result[AccountType.CREATOR]++;
       console.log(`Account_${idx} is the Polyjuice creator.`);
       continue;
     }
 
-    const script = await accounts[idx].getScript();
+    const script = await account.getScript();
     switch (script.code_hash) {
       case META_CONTRACT_CODE_HASH:
         console.log(`Account_${idx} is a meta contract.`);
-        accounts[idx].type = AccountType.META_CONTRACT;
+        account.type = AccountType.META_CONTRACT;
         result[AccountType.META_CONTRACT]++;
         break;
       case L2_SUDT_CONTRACT_CODE_HASH:
         console.log(`Account_${idx} is a sUDT contract.`);
-        accounts[idx].type = AccountType.SUDT_CONTRACT;
+        account.type = AccountType.SUDT_CONTRACT;
         result[AccountType.SUDT_CONTRACT]++;
         break;
       case POLYJUICE_CONTRACT_CODE_HASH:
         console.log(`Account_${idx} is a Polyjuice contract.`);
-        accounts[idx].type = AccountType.POLYJUICE_CONTRACT;
+        account.type = AccountType.POLYJUICE_CONTRACT;
         result[AccountType.POLYJUICE_CONTRACT]++;
         break;
       case ETH_ACCOUNT_LOCK_CODE_HASH:
         console.log(`Account_${idx} is an ETH externally owned account.`);
-        accounts[idx].type = AccountType.ETH_EOA;
+        account.type = AccountType.ETH_EOA;
         result[AccountType.ETH_EOA]++;
         break;
       case TRON_ACCOUNT_LOCK_CODE_HASH:
         console.log(`Account_${idx} is a Tron externally owned account.`);
-        accounts[idx].type = AccountType.TRON_EOA;
+        account.type = AccountType.TRON_EOA;
         result[AccountType.TRON_EOA]++;
         break;
       default:
@@ -181,9 +219,12 @@ async function initAccounts() {
         break;
     }
 
+    if (idx % 1000 === 0) saveAccountData();
     await sleep(INTERVAL + 10);
   }
 
-  logger.info(`Total account num: ${accounts.length}`);
+  logger.info(`Total account num: ${accounts.size}`);
   logger.info(result);
+
+  await saveAccountData();
 })();
