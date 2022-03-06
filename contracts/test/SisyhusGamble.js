a0ec7c891c1823722f955030a6842977e600be25
Flouse <flouse@gmail.com>
2022-03-06 11:00:15 +0000

    reusable-workflow: Run hardhat test --network gw_devnet_v1

--- contracts/test/SisyhusGamble.js
+++ contracts/test/SisyhusGamble.js
@@ -31,7 +31,7 @@ describe("SisyphusGamble", function () {
     expect(await erc20.balanceOf(sender.address)).to.equal(balanceOfSender - 1);
     console.log(`  Getting Sisyphus Gamble Venues...`);
     let gameList = await sisyphusGambleVenues.getSisyphusGambleVenues();
-    // console.log(gameList);
+    console.log("gameList:", gameList);
     let sisyphusGambleAddress = gameList[0].sisyphusGamble;
     console.log(`  Sisyphus gamble venues deployed on address: ${sisyphusGambleAddress}`);
 
@@ -45,7 +45,7 @@ describe("SisyphusGamble", function () {
     await gambleContract.gamble(1);
     expect(await gambleContract.totalPrize()).eq(4);
 
-    console.log("Claim Prize");
+    console.log(">> Claim Prize");
     balanceOfSender = await erc20.balanceOf(sender.address);
     console.log(`  sender's balnace = ${balanceOfSender}`);
 
--- contracts/test/newMemory.js
+++ contracts/test/newMemory.js
@@ -5,10 +5,9 @@ describe("Memory Contract", function () {
   it("Deploy and new some memory", async () => {
     const contractFact = await ethers.getContractFactory("Memory");
     const contract = await contractFact.deploy();
+    // console.log("call:", contractFact.interface.encodeFunctionData("newMemory", [73728]));
 
-    console.log("call:", contractFact.interface.encodeFunctionData("newMemory", [73728]));
     const maxMemory = 3_932_160 / 256; // 1024 * 1024 * 3 = 3,932,160 = 3 MB
-
     for (let step = 1024; step <= maxMemory; step += 1024) {
       let uint8Length = await contract.newMemory(step);
       // console.log("uint8Length = ", parseInt(uint8Length));
