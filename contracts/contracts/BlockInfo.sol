a73cf1dd0dea3ce1aee5da36de082624920abe2c
m-bo-one <bogdankurinniy.dev1@gmail.com>
2022-03-10 12:43:40 +0200

    Add unitests for fetching block info from EVM

---
+++ contracts/contracts/BlockInfo.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract BlockInfo {
+
+    event BlockEvt(
+        uint256 number,
+        bytes32 hash
+    );
+
+    function getCurrentBlockNumber() public view returns (uint256) {
+        return block.number;
+    }
+
+    function getBlockNumber() public view returns (uint256) {
+        return block.number - 1;
+    }
+
+    function getBlockHash(uint256 number) public view returns (bytes32) {
+        return blockhash(number);
+    }
+
+    function executeCurrentBlockHash() public {
+        emit BlockEvt(block.number, blockhash(block.number));
+    }
+
+    function getBlockTimestamp() public view returns (uint256) {
+        return block.timestamp;
+    }
+
+    function getBlockDifficulty() public view returns (uint256) {
+        return block.difficulty;
+    }
+
+    function getBlockCoinbase() public view returns (address) {
+        return block.coinbase;
+    }
+
+    function getChainId() public view returns (uint256 chainId_) {
+        assembly {
+            chainId_ := chainid()
+        }
+    }
+}
