1842605fb12c382355cece37d1ea38b4d52c70b1
magicalne <magicalne@gmail.com>
2022-03-11 16:40:19 +0800

    chore: reformat indentation

--- contracts/test/TotalSupply.js
+++ contracts/test/TotalSupply.js
@@ -11,15 +11,15 @@ const SUDT_DECIMALS = 18;
 const SUDT_TOTAL_SUPPLY = 9999999999;
 
 describe("TotalSupply", function () {
-	it("Deoloy erc20 proxy -> get total supply", async () => {
-		const erc20Fact = await ethers.getContractFactory("contracts/SudtERC20Proxy_UserDefinedDecimals.sol:ERC20");
-		const erc20 = await erc20Fact.deploy(SUDT_NAME, SUDT_SYMBOL, SUDT_TOTAL_SUPPLY, SUDT_ID, SUDT_DECIMALS);
-		console.log(`  ERC20 on address: ${erc20.address}`);
-		const [sender] = await ethers.getSigners();
-		const totalSupply = await erc20.callStatic.totalSupply();
-		expect(totalSupply, SUDT_TOTAL_SUPPLY);
+  it("Deoloy erc20 proxy -> get total supply", async () => {
+    const erc20Fact = await ethers.getContractFactory("contracts/SudtERC20Proxy_UserDefinedDecimals.sol:ERC20");
+    const erc20 = await erc20Fact.deploy(SUDT_NAME, SUDT_SYMBOL, SUDT_TOTAL_SUPPLY, SUDT_ID, SUDT_DECIMALS);
+    console.log(`  ERC20 on address: ${erc20.address}`);
+    const [sender] = await ethers.getSigners();
+    const totalSupply = await erc20.callStatic.totalSupply();
+    expect(totalSupply, SUDT_TOTAL_SUPPLY);
 
-	});
+  });
 });
 /**
  * How to run this?
