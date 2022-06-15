1028f6531c98115094308a976e6b7fe436ab38b6
ShookLyngs <shooklyngs@gmail.com>
2022-06-15 18:17:45 +0800

    feat: use secrets in betanet testing

--- contracts/test/network.js
+++ contracts/test/network.js
@@ -1,11 +1,17 @@
-const hardhat = require("hardhat");
+const { config, network, ethers } = require("hardhat");
 const assert = require("assert");
 
-describe("Check the Godwoken network version", function () {
-  const { network } = hardhat;
-  console.log(network.config);
+describe("Check the Godwoken network version", () => {
+  assert.notEqual(config, undefined, 'should have a config field');
 
-  it("should have a config field", function () {
-    assert.notEqual(hardhat.config, undefined);
-  });
+  const data = JSON.parse(JSON.stringify(network.config));
+  assert(data.accounts.length >= 2, 'should have set 2 accounts');
+
+  const [userOne, userTwo] = data.accounts;
+  assert(userOne !== userTwo, "accounts shouldn't be repeated");
+      
+  // Compute accounts and display network config
+  data.accounts[0] = ethers.utils.computeAddress(data.accounts[0]);
+  data.accounts[1] = ethers.utils.computeAddress(data.accounts[1]);
+  console.log(data);
 });
