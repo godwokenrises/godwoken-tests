a7ef682e4182473f34a8cee8c18e6ffe09f01a15
Flouse <flouse@gmail.com>
2022-06-24 12:05:22 +0800

    Add  sUDT_ERC20_Proxy contract test case for Testnet v1 (#134)
    * Add sUDT_ERC20_Proxy contract test case
    
    * Update the info of sUDT_ERC20_Proxy contracs on Godwoken testnet_v1
    
    * chore: add a default TEST_PK containing some GWK

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -3,11 +3,18 @@ require("@nomiclabs/hardhat-web3");
 
 const INFURA_PROJECT_ID = "719d739434254b88ac95d53e2b6ac997";
 
-// eth_address: 0x966b30e576a4d6731996748b48dd67c94ef29067
-const PRIVATE_KEY = process.env.PRIVATE_KEY ?? "1390c30e5d5867ee7246619173b5922d3b04009cab9e9d91e14506231281a997";
-
-// eth_address: 0x4fef21f1d42e0d23d72100aefe84d555781c31bb
-const PRIVATE_KEY2 = process.env.PRIVATE_KEY2 ?? "2dc6374a2238e414e51874f514b0fa871f8ce0eb1e7ecaa0aed229312ffc91b0";
+const TEST_PK1 = process.env.PRIVATE_KEY ??
+  // eth_address: 0x966b30e576a4d6731996748b48dd67c94ef29067
+  "1390c30e5d5867ee7246619173b5922d3b04009cab9e9d91e14506231281a997";
+const TEST_PK2 = process.env.PRIVATE_KEY2 ??
+  // eth_address: 0x4fef21f1d42e0d23d72100aefe84d555781c31bb 
+  "2dc6374a2238e414e51874f514b0fa871f8ce0eb1e7ecaa0aed229312ffc91b0";
+/**
+ * TEST_PK3 should be an EOA containing some GWK.
+ */
+const TEST_PK3 = process.env.PRIVATE_KEY3 ??
+  // eth_address: 0x0c1efcca2bcb65a532274f3ef24c044ef4ab6d73
+  "dd50cac37ec6dd12539a968c1a2cbedda75bd8724f7bcad486548eaabb87fc8b";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -16,40 +23,40 @@ module.exports = {
   networks: {
     gw_devnet_v1: {
       url: `http://localhost:8024`,
-      accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`],
+      accounts: [`0x${TEST_PK1}`, `0x${TEST_PK2}`],
     },
     gw_testnet_v1: {
       url: `https://godwoken-testnet-v1.ckbapp.dev`,
-      accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`],
+      accounts: [`0x${TEST_PK1}`, `0x${TEST_PK2}`, `0x${TEST_PK3}`],
       version: "1.2.0",
       chainId: 71401,
     },
     // TODO: It is the same as gw_testnet_v1 and this name will be deprecated.
     gw_betanet_v1: {
       url: `https://godwoken-betanet-v1.ckbapp.dev`,
-      accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`],
+      accounts: [`0x${TEST_PK1}`, `0x${TEST_PK2}`],
       version: "1.2.0",
       chainId: 71401,
     },
     gw_alphanet_v1: { // for internal testing
       url: `https://godwoken-alphanet-v1.ckbapp.dev`,
-      accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`],
+      accounts: [`0x${TEST_PK1}`, `0x${TEST_PK2}`],
       version: "1.2.0",
       chainId: 202206,
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${PRIVATE_KEY}`],
+      accounts: [`0x${TEST_PK1}`],
       // gas: 1_000_000_000_000_001, // Infura seems to cap it at 19981536.
       // gasPrice: 1
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${PRIVATE_KEY}`]
+      accounts: [`0x${TEST_PK1}`]
     },
     kovan: {
       url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${PRIVATE_KEY}`]
+      accounts: [`0x${TEST_PK1}`]
     },
     hardhat: {
       hardfork: "berlin"
