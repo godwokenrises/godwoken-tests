3bedea0c69bf73251347949b8e287510e988aab6
Shook <44739165+ShookLyngs@users.noreply.github.com>
2022-06-27 09:48:37 +0800

    refactor(failedTx): remove `gasPrice` in transactions, provide clearer assert messages (#140)

--- contracts/contracts/RevertHandling.sol
+++ contracts/contracts/RevertHandling.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8;
 contract RevertHandling {
     string public message;
 
+    event SetMsg(string message, uint256 amount);
+
     function getMsg() public view returns (string memory) {
         return message;
     }
@@ -10,5 +12,7 @@ contract RevertHandling {
     function setMsg(uint success, string memory _message) public payable {
         require(success == 1, "failed to set message");
         message = _message;
+
+        emit SetMsg(message, msg.value);
     }
 }
