60fd537ca81dd74f81f99767132aaa0d9b70fb55
ShookLyngs <shooklyngs@gmail.com>
2022-07-14 23:00:03 +0800

    feat: provide `get-l2-address` util command in the `account-faucet` script

--- scripts/account-faucet/src/faucet/address.ts
+++ scripts/account-faucet/src/faucet/address.ts
@@ -1,4 +1,4 @@
-import config from '../config';
+import { NetworkConfig } from '../config';
 import { GodwokenWeb3 } from '../godwoken/web3';
 import { generateDepositLock } from '../godwoken/deposit';
 import { utils, Hash, Script, HexString, Address } from '@ckb-lumos/base';
@@ -10,16 +10,11 @@ import crypto from 'crypto';
 // https://github.com/nervosnetwork/godwoken/blob/d6c98d8f8a199b6ec29bc77c5065c1108220bb0a/crates/common/src/builtins.rs#L5
 export const ETH_REGISTRY_ID: number = 2;
 
-// https://github.com/nervosnetwork/lumos/blob/develop/packages/config-manager/src/predefined.ts#L71-L123
-export const lumosOptions = {
-  config: config.lumosConfig,
-};
-
-export async function encodeLayer2DepositAddress(gw: GodwokenWeb3, ckbAddress: Address, ethAddress: HexString): Promise<Address> {
+export async function encodeLayer2DepositAddress(config: NetworkConfig, gw: GodwokenWeb3, ckbAddress: Address, ethAddress: HexString): Promise<Address> {
   const { nodeInfo } = await gw.getNodeInfo();
   const gwRollupTypeHash: Hash = await gw.getRollupTypeHash();
 
-  const ownerLock: Script = parseAddress(ckbAddress, lumosOptions);
+  const ownerLock: Script = parseAddress(ckbAddress, config.lumos);
   const ownerLockHash: Hash = utils.computeScriptHash(ownerLock);
 
   const layer2Lock: Script = {
@@ -33,27 +28,27 @@ export async function encodeLayer2DepositAddress(gw: GodwokenWeb3, ckbAddress: A
     nodeInfo.gwScripts.deposit.typeHash
   );
 
-  return encodeToAddress(depositLock, lumosOptions);
+  return encodeToAddress(depositLock, config.lumos);
 }
 
-export async function privateKeyToLayer2DepositAddress(gw: GodwokenWeb3, privateKey: HexString): Promise<Address> {
-  const ckbAddress = privateKeyToCkbAddress(privateKey);
+export async function privateKeyToLayer2DepositAddress(config: NetworkConfig, gw: GodwokenWeb3, privateKey: HexString): Promise<Address> {
+  const ckbAddress = privateKeyToCkbAddress(config, privateKey);
   const ethAddress = privateKeyToEthAddress(privateKey);
 
-  return encodeLayer2DepositAddress(gw, ckbAddress, ethAddress.toLocaleLowerCase());
+  return encodeLayer2DepositAddress(config, gw, ckbAddress, ethAddress.toLocaleLowerCase());
 }
 
-export function privateKeyToCkbAddress(privateKey: HexString): Address {
+export function privateKeyToCkbAddress(config: NetworkConfig, privateKey: HexString): Address {
   const publicKey = key.privateToPublic(privateKey);
   const publicKeyHash = key.publicKeyToBlake160(publicKey);
-  const scriptConfig = lumosOptions.config.SCRIPTS.SECP256K1_BLAKE160!;
+  const scriptConfig = config.lumos.config.SCRIPTS.SECP256K1_BLAKE160!;
   const script = {
     code_hash: scriptConfig.CODE_HASH,
     hash_type: scriptConfig.HASH_TYPE,
     args: publicKeyHash,
   };
 
-  return encodeToAddress(script, lumosOptions);
+  return encodeToAddress(script, config.lumos);
 }
 
 export function privateKeyToEthAddress(privateKey: HexString): HexString {
--- scripts/account-faucet/src/faucet/command.ts
+++ scripts/account-faucet/src/faucet/command.ts
@@ -1,10 +1,12 @@
-import config from '../config';
+import { Network, networks } from '../config';
 import { Command } from 'commander';
 import { Address, HexString } from '@ckb-lumos/base';
 import { GodwokenWeb3 } from '../godwoken/web3';
 import { claimFaucetForCkbAddress } from './faucet';
 import { encodeLayer2DepositAddress, privateKeyToLayer2DepositAddress, toAddress, toHexString } from './address';
 
+// Default faucet refund address
+// This is also the address that sends the faucet transaction
 const defaultCkbAddress = 'ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqflz4emgssc6nqj4yv3nfv2sca7g9dzhscgmg28x';
 
 export interface FaucetCommand {
@@ -27,18 +29,57 @@ export function setupFaucetCommand(program: Command) {
 
 export async function runFaucet(params: FaucetCommand) {
   const { privateKey, ckbAddress, ethAddress, rpc } = params;
-  if (!privateKey && !(ckbAddress && ethAddress)) {
-    throw new Error('provide either `privateKey` or `ckbAddress` and `ethAddress`');
+  if (!privateKey && !ethAddress) {
+    throw new Error('provide either `privateKey` or `ethAddress`');
   }
 
+  const config = networks[Network.TestnetV1];
   const gw = new GodwokenWeb3(rpc || config.rpc);
 
   let depositAddress: Address;
   if (privateKey) {
-    depositAddress = await privateKeyToLayer2DepositAddress(gw, toHexString(privateKey));
+    depositAddress = await privateKeyToLayer2DepositAddress(config, gw, toHexString(privateKey));
   } else {
-    depositAddress = await encodeLayer2DepositAddress(gw, toAddress(ckbAddress!), toHexString(ethAddress!));
+    depositAddress = await encodeLayer2DepositAddress(config, gw, toAddress(ckbAddress!), toHexString(ethAddress!));
   }
 
   await claimFaucetForCkbAddress(depositAddress);
+}
+
+export function setupCalculateLayer2AddressCommand(program: Command) {
+  program
+    .command('get-l2-address')
+    .description('calculate layer 2 deposit address')
+    .option('-p, --private-key <privateKey>', '[HexString] ckb private key')
+    .option('-e --eth-address <ethAddress>', '[HexString] eth address (optional)')
+    .option('-c --ckb-address <ckbAddress>', '[Address] ckb address (optional)', defaultCkbAddress)
+    .option('-n, --network <network>', '[string] network name', Network.TestnetV1)
+    .action(runCalculateLayer2Address);
+}
+
+export interface CalculateLayer2AddressCommand {
+  privateKey?: HexString | string;
+  ckbAddress?: HexString | string;
+  ethAddress?: HexString | string;
+  network: Network;
+}
+
+async function runCalculateLayer2Address(params: CalculateLayer2AddressCommand) {
+  const { privateKey, ckbAddress, ethAddress, network } = params;
+  if (!privateKey && !ethAddress) {
+    throw new Error('provide either `privateKey` or `ethAddress`');
+  }
+
+  const config = networks[network];
+  const gw = new GodwokenWeb3(config.rpc);
+
+  if (privateKey) {
+    console.log(
+      await privateKeyToLayer2DepositAddress(config, gw, toHexString(privateKey))
+    );
+  } else {
+    console.log(
+      await encodeLayer2DepositAddress(config, gw, toAddress(ckbAddress!), toHexString(ethAddress!))
+    );
+  }
 }
