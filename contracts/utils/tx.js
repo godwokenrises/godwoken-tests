a31cf0b293b82fd056ed8bb36030a33eca0f87fe
sunchengzhu <sunchengzhu945@gmail.com>
2022-09-28 19:59:50 +0800

    remove not use function

--- contracts/utils/tx.js
+++ contracts/utils/tx.js
@@ -1,5 +1,3 @@
-const {ethers} = require("hardhat");
-
 async function getTxReceipt(provider, txHash, count) {
     let response
     for (let i = 0; i < count; i++) {
@@ -25,44 +23,11 @@ async function getGasPrice(provider) {
     return gasPrice.toHexString().replaceAll("0x0", "0x");
 }
 
-function BigInterToHexString(bn) {
-    if (bn < 16) {
-        return "0x" + bn.toHexString().replaceAll("0x0", "");
-    }
-    return bn.toHexString().replaceAll("0x0", "0x");
-}
-
-async function sendTxCount(provider, count) {
-    let contractAddress = await ethers.provider.getSigner(0).getAddress()
-    for (let i = 0; i < count; i++) {
-        let txCount = provider.getTransactionCount(contractAddress);
-        console.log(contractAddress, " count:", await txCount)
-        await sendRandomTx(provider)
-    }
-    console.log("send successful:", count)
-}
-
-async function sendTxToAddBlockNum(provider, blockNum) {
-
-    let endNum = await provider.getBlockNumber() + blockNum;
-    let currentNum = await provider.getBlockNumber();
-    console.log("currentNum:", currentNum, " end:", endNum)
-    let contractAddress = await ethers.provider.getSigner(0).getAddress()
-    while (currentNum < endNum) {
-        let txCount = provider.getTransactionCount(contractAddress);
-        console.log(contractAddress, " count:", await txCount)
-        await sendRandomTx(provider)
-        currentNum = await provider.getBlockNumber();
-        console.log("currentNum:", currentNum, " end:", endNum)
-    }
-    console.log("curren block num:", endNum)
-}
-
 async function getTxCount(provider, address) {
     let loopMaxTime = 100000;
     while (true) {
         loopMaxTime--
-        if(loopMaxTime<0){
+        if (loopMaxTime < 0) {
             return
         }
         let pending_count = provider.getTransactionCount(address, "pending")
@@ -72,19 +37,7 @@ async function getTxCount(provider, address) {
     }
 }
 
-async function sendRandomTx(provider) {
-    let from = (await ethers.getSigners())[1].address
-    let logContract = await ethers.getContractFactory("opcode_assembly_log");
-    try {
-        await provider.send("eth_sendTransaction", [{
-            "from": from,
-            "data": logContract.bytecode
-        }]);
-    } catch (e) {
-    }
-}
-
-async function sleep(timeOut){
+async function sleep(timeOut) {
     await new Promise(r => setTimeout(r, timeOut));
 }
 
@@ -93,7 +46,4 @@ module.exports = {
     getTxCount,
     getTxReceipt,
     getGasPrice,
-    // BigInterToHexString,
-    // sendTxToAddBlockNum,
-    // sendTxCount
 };
