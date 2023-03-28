4310d4b47b0cc40602e19b566947801871563591
magicalne <magicalne@gmail.com>
2023-03-28 16:32:58 +0800

    chore: fix typo

--- contracts/scripts/before-axon-devnet.js
+++ contracts/scripts/before-axon-devnet.js
@@ -5,18 +5,18 @@ const hardhat = require("hardhat");
 async function main() {
     const MIN_BALANCE = 100_000_000_000;
     const ethers = hardhat.ethers;
-    const [a1, a2, a3, a4, genersis] = await ethers.getSigners();
+    const [a1, a2, a3, a4, genesis] = await ethers.getSigners();
     if (await a1.getBalance() < MIN_BALANCE) {
-        await genersis.sendTransaction({to: a1.address, value: MIN_BALANCE})
+        await genesis.sendTransaction({to: a1.address, value: MIN_BALANCE})
     }
     if (await a2.getBalance() < MIN_BALANCE) {
-        await genersis.sendTransaction({to: a2.address, value: MIN_BALANCE})
+        await genesis.sendTransaction({to: a2.address, value: MIN_BALANCE})
     }
     if (await a3.getBalance() < MIN_BALANCE) {
-        await genersis.sendTransaction({to: a3.address, value: MIN_BALANCE})
+        await genesis.sendTransaction({to: a3.address, value: MIN_BALANCE})
     }
     if (await a4.getBalance() < MIN_BALANCE) {
-        await genersis.sendTransaction({to: a4.address, value: MIN_BALANCE})
+        await genesis.sendTransaction({to: a4.address, value: MIN_BALANCE})
     }
 }
 main()
