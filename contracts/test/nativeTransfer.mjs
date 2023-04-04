b09774a5f1df802fe02f41a18593e4ae9b552d59
Flouse <1297478+Flouse@users.noreply.github.com>
2023-04-04 19:02:03 +0800

    Update contracts/test/nativeTransfer.mjs

--- contracts/test/nativeTransfer.mjs
+++ contracts/test/nativeTransfer.mjs
@@ -106,6 +106,7 @@ describe("transfer failed", function () {
         }
     }).timeout(15000)
 
+    // TODO: remove `skip` after deploying Godwoken v1.13+.
     it.skip("gasLimit not enough but enough for 21000", async () => {
         to = ethers.Wallet.createRandom().address;
         const from_balance = await ethers.provider.getBalance(from)
