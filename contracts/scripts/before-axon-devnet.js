3ab7ef6fd6b826869639d271f9f8986598e6d465
Flouse <1297478+Flouse@users.noreply.github.com>
2023-08-04 14:59:24 +0000

    refactor(axon-tests): query the balance of every test account

--- contracts/scripts/before-axon-devnet.js
+++ contracts/scripts/before-axon-devnet.js
@@ -1,23 +1,31 @@
-const hardhat = require("hardhat");
+const { ethers } = require("hardhat");
 
 /*
-    * Deposit test accounts before running test.
-*/
+ * Deposit for test accounts before running tests.
+ */
 async function main() {
   const MIN_BALANCE = 100_000_000_000;
-  const ethers = hardhat.ethers;
-  const [a1, a2, genesis, a3, a4] = await ethers.getSigners();
-  if (await a1.getBalance() < MIN_BALANCE) {
-    await genesis.sendTransaction({ to: a1.address, value: MIN_BALANCE })
+  const signers = await ethers.getSigners();
+
+  // Query the balance of every test account
+  let [b1, b2, _bg, b3, b4] = signers.map(async account => {
+    const balance = await account.getBalance();
+    console.log(`native token balance of ${account.address}: ${balance.toString()}`);
+    return balance;
+  });
+
+  const [a1, a2, genesis, a3, a4] = signers;
+  if (await b1 < MIN_BALANCE) {
+    await genesis.sendTransaction({ to: a1.address, value: MIN_BALANCE });
   }
-  if (await a2.getBalance() < MIN_BALANCE) {
-    await genesis.sendTransaction({ to: a2.address, value: MIN_BALANCE })
+  if (await b2 < MIN_BALANCE) {
+    await genesis.sendTransaction({ to: a2.address, value: MIN_BALANCE });
   }
-  if (await a3.getBalance() < MIN_BALANCE) {
-    await genesis.sendTransaction({ to: a3.address, value: MIN_BALANCE })
+  if (await b3 < MIN_BALANCE) {
+    await genesis.sendTransaction({ to: a3.address, value: MIN_BALANCE });
   }
-  if (await a4.getBalance() < MIN_BALANCE) {
-    await genesis.sendTransaction({ to: a4.address, value: MIN_BALANCE })
+  if (await b4 < MIN_BALANCE) {
+    await genesis.sendTransaction({ to: a4.address, value: MIN_BALANCE });
   }
 }
 
@@ -27,7 +35,8 @@ main()
     console.error(error);
     process.exit(1);
   });
+
 /**
  * How to run this?
- * > npx hardhat run scripts/before-axon-devnet.js --network axon_devnet
+ * > npx hardhat run scripts/before-axon-devnet.js --network axon_alphanet
  */
